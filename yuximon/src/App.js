3bb51227e5caf6b0038febbb4f1c48ce820c3031
jdanielue <jdurregoe@gmail.com>
2021-11-01 09:54:11 -0500

    added function component informacion

--- yuximon/src/App.js
+++ yuximon/src/App.js
@@ -1,26 +1,51 @@
 import { useEffect, useState, useRef } from "react";
 import { Fragment } from "react";
-import { getPokemons } from "./services/fetchinPokeapi"
+import axios from "axios";
+import { getPokemons } from "./services/fetchinPokeapi";
 import './App.css';
+
+
+  //custom hook
+  const useRequest = (url) => {
+    const [loading, setLoading] = useState(true);
+    const [pokemonsList, setPokemonsList] = useState([]);
+
+    //axios
+    useEffect(() => {
+      const getData = async () => {
+        try {
+          const {
+            data:{
+              results
+            }
+          } = await axios.get(url);
+          console.log(results);
+          setLoading(false);
+          setPokemonsList(results);
+        } catch (error) {
+        }
+      };
+      setTimeout(() => {
+        getData()
+      }, 1000)
+    }, [])
+    return {
+      loading,
+      pokemonsList
+    };
+  };
 
 
 function App() {
   const pokemonInput = useRef()
-  const pokemonsToCallAPI = useRef()
-  const [pokemonsList, setpokemonsList] = useState([]);
   const [pokemonsCaught, setpokemonsCaught] = useState([]);
+  const [infopokemon, setInfopokemon] = useState('');
 
-  const Info = (pos) => {
-    console.log(pos)
-  }
+  const {
+    loading,
+    pokemonsList
+  } = useRequest('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150');
 
-  const CallTheAPI = async () => {
-    const numero = pokemonsToCallAPI.current.value;
-    let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit='
-    const pokeList = await getPokemons(url, numero)
-    setpokemonsList(pokeList)
-    pokemonsToCallAPI.current.value = null
-    }
 
   const findPokemon = () => {
     const pokemonTyped = pokemonInput.current.value;
@@ -28,13 +53,13 @@ function App() {
       return
     }
 
-    pokemonsList.forEach((pokemon) => {
-      if (pokemon.name === pokemonTyped) {
-        setpokemonsCaught((pevpokes) => {
-          return [... pevpokes, pokemonTyped]
-        })
-      }
-    });
+    const pokemon = pokemonsList.find((pokemon) => pokemon.name === pokemonTyped);
+
+    if (pokemon) {
+       setpokemonsCaught((pevpokes) => {
+        return [... pevpokes, pokemon]
+      })
+    }
     pokemonInput.current.value = null
   }
   const freePokemon = () => {
@@ -42,11 +67,24 @@ function App() {
       return []
     })
   }
+
+  const Informacion = (pokemon) => {
+    console.log(pokemon);
+    return (
+        <h3>
+          {pokemon}
+        </h3>
+    )
+  }
+
   return (
   <Fragment>
+    {
+    loading ? <h1>loading</h1> : <h1> Pokemons charged</h1>
+    }
     <h1>Pokemon ... Gotta Catch Em All</h1>
-    <input ref={pokemonsToCallAPI} type="text" placeholder="Type here how many pokemons do you wanna see"></input>
-    <button onClick={CallTheAPI}>call the API</button>
+    {/* <input ref={pokemonsToCallAPI} type="text" placeholder="Type here how many pokemons do you wanna see"></input>
+    <button onClick={CallTheAPI}>call the API</button> */}
     <h2>Pokemons to catch {pokemonsList.length - pokemonsCaught.length}</h2>
     <div className="container">
       <ul>
@@ -59,8 +97,11 @@ function App() {
     <button onClick={findPokemon}>capture</button>
     <div className="container2">
       <ul>
-       {pokemonsCaught.map((item, pos) => <li key={item} onClick={(item)=>Info(item)} >{item}</li>)}
+       {pokemonsCaught.map((pokemon, pos) => <li key={pokemon.name}>{pokemon.name}<button onClick={() => Informacion(pokemon.url)}>info</button></li>)}
      </ul>
+    </div>
+    <Informacion/>
+    <div>
     </div>
   </Fragment>
   );
